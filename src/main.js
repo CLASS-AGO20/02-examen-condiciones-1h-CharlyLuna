3624932bfa055f005b9192cf1c869c4bad3897e3
CharlyLuna <cluna10@ucol.mx>
2020-11-26 15:16:35 -0600

    Implemented convertir

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 export default class App {
 
+    convertir(pesos) {
+        if(pesos < 2000) {
+            return pesos * 20.5;
+        } else {
+            return pesos * 22.7;
+        }
+    }
 }
 
 let app = new App();
+
+// Comprobacion convertir
+console.log(app.convertir(1000));
+console.log(app.convertir(2000));
+console.log(app.convertir(2500));
